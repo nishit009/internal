7b6c6ae8d4dc0b3ac481fbd41f4dda67006bae6f
nishit009 <nishit.nishu123@gmail.com>
2024-12-31 11:18:41 +0530

    internal

---
+++ src/test/java/app/maven/kmit/Sample_internal/AppTest.java
@@ -0,0 +1,20 @@
+package app.maven.kmit.Sample_internal;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
